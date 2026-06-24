61522d2bfeda749a7eec81a69d77d05ca46b12fa
zaq15csdn <zhangaiqiang1@huawei.com>
2026-06-24 21:39:51 +0800

    [Dot](test) Re-enable tf32/hf32 dot tests
    Remove skip/xfail marks on test_dot_2, test_dot_2_allow_tf32 and
    test_dot_2_input_tf32 now that the TA backend supports allow_tf32 /
    input_precision="tf32".

--- third_party/ascend/unittest/pytest_ut/test_dot.py
+++ third_party/ascend/unittest/pytest_ut/test_dot.py
@@ -125,7 +125,6 @@ typelist = [
 ]
 
 
-@pytest.mark.skip(reason="not supported after the NPUIR is updated in April, and will be fixed later")
 @pytest.mark.parametrize("B, C, D", testlist2)
 @pytest.mark.parametrize("sigtype", typelist)
 def test_dot_2(restore_npu_hf32_setting, sigtype, B, C, D):
@@ -137,9 +136,6 @@ def test_dot_2(restore_npu_hf32_setting, sigtype, B, C, D):
     test_common.validate_cmp(sigtype, z, z_ref)
 
 
-@pytest.mark.xfail(
-    reason="Temporarily disabled: TA backend does not support allow_tf32 yet. Will be fixed in follow-up."
-)
 @pytest.mark.parametrize("B, C, D", testlist2)
 @pytest.mark.parametrize("sigtype", typelist)
 def test_dot_2_allow_tf32(restore_npu_hf32_setting, sigtype, B, C, D):
@@ -151,7 +147,6 @@ def test_dot_2_allow_tf32(restore_npu_hf32_setting, sigtype, B, C, D):
     test_common.validate_cmp(sigtype, z, z_ref)
 
 
-@pytest.mark.skip(reason="not supported after the NPUIR is updated in April, and will be fixed later")
 @pytest.mark.parametrize("B, C, D", testlist2)
 @pytest.mark.parametrize("sigtype", typelist)
 def test_dot_2_input_tf32(restore_npu_hf32_setting, sigtype, B, C, D):
